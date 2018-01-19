1f752a1c73271f43859e5b15b853edf030fa8690
Thomas Lazar <thomas.lazar@shs-viveon.com>
2018-01-19 11:15:17 +0100

    Add project files.

---
+++ SyntaxFuckeryCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyntaxFuckeryCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
